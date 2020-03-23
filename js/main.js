7d78dda9862f981b990e4e13b6c23ee520410112
sweetseonah <salj3895@gmail.com>
2020-03-23 18:08:43 +0900

    2020.03.23

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
-
-$(document).ready(function(){
-  $('.preview').click(fuction(){
-      $('.slider').bxSlider();
-    });
-});
+   // 
+   // var menuRight = document.getElementById( 'cbp-spmenu-s2' ),
+   //      body = document.body;
+   //
+   //  showRight.onclick = function() {
+   //    classie.toggle( this, 'active' );
+   //    classie.toggle( menuRight, 'cbp-spmenu-open' );
+   //    disableOther( 'showRight' );
+   //  };
+   //  function disableOther( button ) {
+   //    if( button !== 'showRight' ) {
+   //      classie.toggle( showRight, 'disabled' );
+   //    }
+   //  };

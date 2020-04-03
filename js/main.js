1f41803f6a87fe428205dcd22b0c1c2a580bc7a3
sweetseonah <salj3895@gmail.com>
2020-04-03 17:35:40 +0900

    2020.04.03

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
-$(document).ready(function(){
+const carouselSlide = document.querySelector('.slide');
+const carouselImages = document.querySelectorAll('.slideBox img');
 
+const prevBtn = document.querySelector('#prevBtn');
+const nextBrn = document.querySelector('#nextBtn');
 
+//counter
+let counter = 1;
+const size = carouselImages[0].clientWidth;
 
-  
+carouselslide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+//button listeners
+
+nextBtn.addEventlistner('click',()=>{
+  carouselSlide.style.transition = "transform 0.4s ease-in-out";
+  counter++;
+  carouselSlide.style.transform = 'transl'
 });
